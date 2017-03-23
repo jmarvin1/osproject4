c92aba8e6a342682ecbf848a02c108e45e162491
James Marvin <jmarvin1@nd.edu>
2017-03-22 22:37:50 -0400

    careless whisper

--- project4/parse.cpp
+++ project4/parse.cpp
@@ -2,6 +2,8 @@
 #include<fstream>
 #include<sstream>
 #include<string>
+#include<vector>
+#include "parse.h"
 using namespace std;
 
 int main(int argc, char* argv[])
@@ -22,31 +24,61 @@ int main(int argc, char* argv[])
 		{
 			//cout<<line.substr(line.find(delimeter)+1)<<endl;
 			per=stoi(line.substr(line.find(delimeter)+1));
-			cout<<per<<endl;
+			cout<<per;
 		}
 		else if(numf.compare(line.substr(0,line.find(delimeter)))==0)
 		{
 			nf=stoi(line.substr(line.find(delimeter)+1));
-			cout<<nf<<endl;
+			cout<<nf;
 		}
 		else if(nump.compare(line.substr(0,line.find(delimeter)))==0)
 		{
 			np=stoi(line.substr(line.find(delimeter)+1));
-			cout<<np<<endl;
+			cout<<np;
 		}
 		else if(sf.compare(line.substr(0,line.find(delimeter)))==0)
 		{
 			searf=line.substr(line.find(delimeter)+1);
-			cout<<searf<<endl;
+			cout<<searf;
 		}
 		else if(sif.compare(line.substr(0,line.find(delimeter)))==0)
 		{
 			sitf=line.substr(line.find(delimeter)+1);
-			cout<<sitf<<endl;
+			cout<<sitf;
 		}
 	}
+	cout<<endl;
 	
+	vector<string> test;
+	test=parseFile(searf);
+	
+	for(int i=0; i<test.size(); i++)
+	{
+		cout<<test[i];
+	}
+	cout<<endl;
+	
+	vector<string> test1;
+	test1=parseFile(sitf);
+	for(int i=0; i<test1.size(); i++)
+	{
+		cout<<test1[i];
+	}
+	cout<<endl;
 	
 
 }
+vector<string> parseFile(string filename)
+{
+
+	vector<string> searchTerms;
+	ifstream infile(filename);
+	string line;
+	while(getline(infile,line)){
+		searchTerms.push_back(line);
+	}
+
+	return searchTerms;
+}
+
 
