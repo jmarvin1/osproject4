600769e562ffe0c862204a1f030b6aed468fd9ad
J. Patrick Lacher <jlacher1@nd.edu>
2017-04-29 14:06:39 -0400

    fs_format and fs_mount

--- project6/fs.c
+++ project6/fs.c
@@ -1,4 +1,4 @@
-
+// ---------- Source Files ----------
 #include "fs.h"
 #include "disk.h"
 
@@ -8,10 +8,20 @@
 #include <errno.h>
 #include <unistd.h>
 
+// ---------- Macros ----------
+
 #define FS_MAGIC           0xf0f03410
 #define INODES_PER_BLOCK   128
 #define POINTERS_PER_INODE 5
 #define POINTERS_PER_BLOCK 1024
+
+// ---------- Global Variables ----------
+
+char * free_block_bitmap = 0; // Usage:
+									//	1 - block is in use
+									//	0 - block is empty
+
+// ---------- Data Structures ----------
 
 struct fs_superblock {
 	int magic;
@@ -34,13 +44,39 @@ union fs_block {
 	char data[DISK_BLOCK_SIZE];
 };
 
+// ---------- Primary FS Functions ----------
+
 int fs_format()
 {
-	return 0;
+	// --- Check for Existing Mounted Disk ---
+	if (free_block_bitmap) {
+		return 0;
+	}
+	// --- Create New Superblock ---
+	union fs_block superblock;
+	superblock.super.magic = FS_MAGIC;
+	superblock.super.nblocks = disk_size();
+	superblock.super.ninodeblocks = superblock.super.nblocks/10;
+	superblock.super.ninodes = superblock.super.ninodeblocks * INODES_PER_BLOCK;
+	// --- Write Superblock to Disk --- 
+	disk_write(0, superblock.data);
+	// --- Create Empty Inode Block ---
+	union fs_block emptyinode;
+	int i;
+	for (i=0; i < INODES_PER_BLOCK; i++) {
+		emptyinode.inode[i].isvalid = 0;
+	}
+	// --- Write Empty Inode Block into Every Allocated Inode Block ---
+	for (i=1; i <= superblock.super.ninodeblocks; i++) {
+		disk_write(i, emptyinode.data);
+	}
+	// --- Exit Successfully ---
+	return 1;
 }
 
 void fs_debug()
 {
+	// --- Load Superblock ---
 	union fs_block superblock;
 	disk_read(0,superblock.data);
 	printf("superblock:\n");
@@ -53,27 +89,31 @@ void fs_debug()
 	printf("    %d blocks\n",superblock.super.nblocks);
 	printf("    %d inode blocks\n",superblock.super.ninodeblocks);
 	printf("    %d inodes\n",superblock.super.ninodes);
-	
+	// --- Load Inode Blocks ---
 	int i, j, k;
 	union fs_block block;
 	union fs_block indirect;
 	for (i=1; i <= superblock.super.ninodeblocks; i++) {
 		disk_read(i, block.data);
 		for (j=0; j < INODES_PER_BLOCK; j++) {
+			// --- Only Print Valid Inodes ---
 			if (block.inode[j].isvalid) {
 				printf("inode %d\n", j);
 				printf("    size: %d bytes\n", block.inode[j].size);
 				printf("    direct blocks: ");
+				// --- Print Valid Direct Pointers ---
 				for (k=0; k < POINTERS_PER_INODE; k++) {
 					if (block.inode[j].direct[k]) {
 						printf("%d ", block.inode[j].direct[k]);
 					}
 				}
 				printf("\n");
+				// --- Follow Indirect Pointer To Find Pointers in Indirect Data Block ---
 				if (block.inode[j].indirect) {
 					printf("    indirect: %d\n", block.inode[j].indirect);
 					disk_read(block.inode[j].indirect, indirect.data);
 					printf("    indirect data blocks: ");
+					// --- Print Valid Indirect Pointers ---
 					for(k=0; k < POINTERS_PER_BLOCK; k++) {
 						if (indirect.pointers[k]){
 							printf("%d ", indirect.pointers[k]);
@@ -88,7 +128,51 @@ void fs_debug()
 
 int fs_mount()
 {
-	return 0;
+	// --- Read Superblock ---
+	union fs_block superblock;
+	disk_read(0, superblock.data);
+	// --- Exit if Magic Number Invalid ---
+	if (superblock.super.magic != FS_MAGIC) {
+		return 0;
+	}
+	// --- Allocate Free Block Bitmap ---
+	free_block_bitmap = malloc(sizeof(char) * superblock.super.nblocks);
+	free_block_bitmap[0] = 1;
+	int i, j, k;
+	for (i = 1; i < superblock.super.nblocks; i++) {
+		free_block_bitmap[i] = 0;
+	}
+	// --- Scan Through Inodes to Fill Free Block Bitmap ---
+	union fs_block block;
+	union fs_block indirect;
+	for (i=1; i <= superblock.super.ninodeblocks; i++) {
+		free_block_bitmap[i] = 1;
+		disk_read(i, block.data);
+		for (j=0; j < INODES_PER_BLOCK; j++) {
+			// --- Only Examine Valid Inodes ---
+			if (block.inode[j].isvalid) {
+				// --- Mark Valid Direct Pointers ---
+				for (k=0; k < POINTERS_PER_INODE; k++) {
+					if (block.inode[j].direct[k]) {
+						free_block_bitmap[block.inode[j].direct[k]] = 1;
+					}
+				}
+				// --- Follow Indirect Pointer To Find Pointers in Indirect Data Block ---
+				if (block.inode[j].indirect) {
+					free_block_bitmap[block.inode[j].indirect] = 1;
+					disk_read(block.inode[j].indirect, indirect.data);
+					// --- Print Valid Indirect Pointers ---
+					for(k=0; k < POINTERS_PER_BLOCK; k++) {
+						if (indirect.pointers[k]){
+							free_block_bitmap[indirect.pointers[k]] = 1;
+						}
+					}
+				}
+			}
+		}
+	}
+	// --- Return Successfully ---
+	return 1;
 }
 
 int fs_create()
