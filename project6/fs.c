0c41008e2e9b69a835fb7f37577b10f2403ef444
J. Patrick Lacher <jlacher1@nd.edu>
2017-04-29 13:09:26 -0400

    fs_debug

--- project6/fs.c
+++ project6/fs.c
@@ -41,14 +41,49 @@ int fs_format()
 
 void fs_debug()
 {
-	union fs_block block;
-	
-	disk_read(0,block.data);
-	
+	union fs_block superblock;
+	disk_read(0,superblock.data);
 	printf("superblock:\n");
-	printf("    %d blocks\n",block.super.nblocks);
-	printf("    %d inode blocks\n",block.super.ninodeblocks);
-	printf("    %d inodes\n",block.super.ninodes);
+	if (superblock.super.magic == FS_MAGIC) {
+		printf("    magic number is valid\n");
+	}
+	else {
+		printf("    magic number is invalid\n");
+	}
+	printf("    %d blocks\n",superblock.super.nblocks);
+	printf("    %d inode blocks\n",superblock.super.ninodeblocks);
+	printf("    %d inodes\n",superblock.super.ninodes);
+	
+	int i, j, k;
+	union fs_block block;
+	union fs_block indirect;
+	for (i=1; i <= superblock.super.ninodeblocks; i++) {
+		disk_read(i, block.data);
+		for (j=0; j < INODES_PER_BLOCK; j++) {
+			if (block.inode[j].isvalid) {
+				printf("inode %d\n", j);
+				printf("    size: %d bytes\n", block.inode[j].size);
+				printf("    direct blocks: ");
+				for (k=0; k < POINTERS_PER_INODE; k++) {
+					if (block.inode[j].direct[k]) {
+						printf("%d ", block.inode[j].direct[k]);
+					}
+				}
+				printf("\n");
+				if (block.inode[j].indirect) {
+					printf("    indirect: %d\n", block.inode[j].indirect);
+					disk_read(block.inode[j].indirect, indirect.data);
+					printf("    indirect data blocks: ");
+					for(k=0; k < POINTERS_PER_BLOCK; k++) {
+						if (indirect.pointers[k]){
+							printf("%d ", indirect.pointers[k]);
+						}
+					}
+					printf("\n");
+				}
+			}
+		}
+	}
 }
 
 int fs_mount()
